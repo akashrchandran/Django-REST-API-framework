da2288b04378346da3f5bb5cd2e2e4369734a5fa
Akash R Chandran <chandranrakash@gmail.com>
2023-05-29 00:55:15 +0530

    updated static url

--- django_rest_trial/settings.py
+++ django_rest_trial/settings.py
@@ -119,7 +119,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
